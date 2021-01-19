55e036be6b467d97c6d6ce62acfdc6551d078cc5
HassanDev13 <hassantupe1999@gmail.com>
2021-01-19 22:20:48 +0000

    update

--- api-routes.js
+++ api-routes.js
@@ -7,5 +7,19 @@ router.get('/', function (req, res) {
         message: 'Welcome to RESTHub crafted with love!'
     });
 });
+// Import contact controller
+var contactController = require('./contactController');
+
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+    
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
 // Export API routes
 module.exports = router;
